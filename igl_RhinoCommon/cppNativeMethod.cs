93c2cc02e0b01d45c0dff20215211a88472b7b5d
xArthur <xliotx@gmail.com>
2021-03-04 10:47:43 +0100

    mod: use relative path for the cpp dll.

--- igl_RhinoCommon/cppNativeMethod.cs
+++ igl_RhinoCommon/cppNativeMethod.cs
@@ -6,8 +6,9 @@ namespace IGLRhinoCommon
 {
     internal static class Import
     {
-        //public const string lib = @"C:\Users\xarthur\source\repos\gh-igl\x64\Debug\igl_cppPort.dll";
-        public const string lib = @"C:\Users\xarthur\source\repos\igl-grasshopper\x64\Release\igl_cppPort.dll";
+        //public const string lib = @"C:\Users\xarthur\source\repos\igl-grasshopper\x64\Release\igl_cppPort.dll";
+        // use relative path
+        public const string cppLib = @"igl_cppPort.dll";
     }
 
     internal static class CppIGL
@@ -15,23 +16,23 @@ namespace IGLRhinoCommon
         /// <summary>
         /// Sums two numbers
         /// </summary>
-        [DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern double Add(double a, double b);
 
         /// <summary>
         /// Compute mesh adjacency list
         /// </summary>
-        [DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void igl_adjacency_list(IntPtr F, int nF, IntPtr adjLstFromCpp, out int sz);
 
-        [DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void igl_boundary_loop(IntPtr F, int nF, IntPtr bndLoopFromCpp, out int sz);
 
-        [DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void extractIsoLinePts(IntPtr V, int nV, IntPtr F, int nF,
             IntPtr con_idx, IntPtr con_val, int numCon, int divN, IntPtr isoLnPts, IntPtr numPtEachLst);
 
-        [DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void computeLaplacian(IntPtr V, int nV, IntPtr F, int nF,
             IntPtr con_idx, IntPtr con_val, int numCon, IntPtr laplacianValue);
     }
