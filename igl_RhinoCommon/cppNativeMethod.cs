3b8b9e9149e84f62e15c1c041b7c1720474b41f7
xArthur <xliotx@gmail.com>
2021-03-04 10:49:59 +0100

    mod: add summary.

--- igl_RhinoCommon/cppNativeMethod.cs
+++ igl_RhinoCommon/cppNativeMethod.cs
@@ -25,13 +25,22 @@ namespace IGLRhinoCommon
         [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void igl_adjacency_list(IntPtr F, int nF, IntPtr adjLstFromCpp, out int sz);
 
+        /// <summary>
+        /// Compute mesh boundary loop
+        /// </summary>
         [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void igl_boundary_loop(IntPtr F, int nF, IntPtr bndLoopFromCpp, out int sz);
 
+        /// <summary>
+        /// Extract mesh IsoLine Points
+        /// </summary>
         [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void extractIsoLinePts(IntPtr V, int nV, IntPtr F, int nF,
             IntPtr con_idx, IntPtr con_val, int numCon, int divN, IntPtr isoLnPts, IntPtr numPtEachLst);
 
+        /// <summary>
+        /// Compute mesh Laplacian
+        /// </summary>
         [DllImport(Import.cppLib, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void computeLaplacian(IntPtr V, int nV, IntPtr F, int nF,
             IntPtr con_idx, IntPtr con_val, int numCon, IntPtr laplacianValue);
