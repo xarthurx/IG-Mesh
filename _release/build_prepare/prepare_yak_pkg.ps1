fc3260b24936f80ffd8b2b493a27fd205f0db399
xTree <xliotx@gmail.com>
2025-10-24 12:43:27 +0200

    update to 0.4.1 with re-written bottom layer for cross-platform support.

--- _release/build_prepare/prepare_yak_pkg.ps1
+++ _release/build_prepare/prepare_yak_pkg.ps1
@@ -7,7 +7,7 @@ if (Test-Path "manifest.yml")
 Copy-Item -Path "..\..\bin\*.dll" -Destination ".\" -Recurse
 Copy-Item -Path "..\..\bin\*.gha" -Destination ".\" -Recurse
 
-&'C:\Program Files\Rhino 7\System\Yak.exe' spec
+&'C:\Program Files\Rhino 8\System\Yak.exe' spec
 
 # Add-Content manifest.yml "`nicon: `"https://i.imgur.com/WABE4LN.png`""
 Add-Content manifest.yml "`nicon: packageManagerIcon.png"
